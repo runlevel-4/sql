bce462856407cd7329ee6c8656faafd1a3197684
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-11-09 20:55:36 -0500

    Create instance_info.sql

---
+++ MSSQL/instance_info.sql
@@ -0,0 +1,8 @@
+----------------------------------------------------------------------
+-- This will gather the server name and version of the SQL instance --
+--                                                                  --
+-- Created: 11/09/2021                                              --
+-- Version: 1.0                                                     --
+----------------------------------------------------------------------
+
+SELECT @@SERVER, @@VERSION
